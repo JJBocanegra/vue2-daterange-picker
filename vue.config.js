84bcb587b72b28ebeb6c4c75f5e3ea994079aeee
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2020-07-06 12:11:07 +0200

    ➖ remove moment dependency

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,2 @@
 module.exports = {
-  // configureWebpack: config => {
-  //   config.externals = {
-  //     ...config.externals,
-  //       ...process.env.NODE_ENV === 'test' ? null:{'moment': 'moment'}
-  //   }
-  // }
 }
